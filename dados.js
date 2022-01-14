21f469480ff6e323e8941325b975caacd22158f0
bs6096098 <96058528+bs6096098@users.noreply.github.com>
2022-01-14 09:31:40 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,12 +1,14 @@
-var quantidade = 10;
-var preco = 6.4;
-var imposto = 1.5;
-var precoFinal = preco + imposto;
+//Conceito de variaveis e manipulação das mesmas
+var quantidade = 10; // Uma variavel que representa a quantidade de um determinado produto
+var preco = 6.4; // O preço do produto sem imposto 
+var imposto = 1.5; //Valor do imposto sobre o produto 
+var precoFinal = preco + imposto; //Preço final com imposto
 
 var nome = "Caneta"
 
 var nome = "Caneta BIC"
 
+//Console log para exibir os resultados 
 console.log(nome)
 console.log(quantidade) 
 console.log(preco)
